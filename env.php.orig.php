011aa41860795e4b38da7739239b728188bad1d6
Rudie Dirkx <github@hotblocks.nl>
2018-12-28 20:50:27 +0100

    base uri+

--- env.php.orig.php
+++ env.php.orig.php
@@ -2,7 +2,7 @@
 
 const CRONLOG_DB_DIR = __DIR__ . '/db';
 
-const CRONLOG_URI = 'https://cronlog.example.com/';
+const CRONLOG_URI = 'https://cronlog.example.com';
 
 const CRONLOG_DELETE_IMPORTS = true;
 const CRONLOG_EMAIL_RESULTS = null;
