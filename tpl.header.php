c9edc07512784158637e24d75278310b82bfd39e
Rudie Dirkx <github@hotblocks.nl>
2022-11-23 02:54:46 +0100

    fixes #18: dark mode

--- tpl.header.php
+++ tpl.header.php
@@ -11,57 +11,7 @@ header('Content-type: text/html; charset=utf-8');
 <meta charset="utf-8" />
 <title>Cronlog</title>
 <link rel="shortcut icon" href="/favicon.png" />
-<style>
-body {
-	font-family: sans-serif;
-}
-table {
-	border-collapse: collapse;
-}
-th, td {
-	padding: 6px 9px;
-	vertical-align: top;
-	border: solid 1px #ddd;
-}
-th:not([align]) {
-	text-align: left;
-}
-tbody tr:nth-child(odd) {
-	background-color: #eee;
-}
-tbody tr.even-section:nth-child(odd) {
-	background-color: hsl(39, 100%, 90%);
-}
-tbody tr.even-section:nth-child(even) {
-	background-color: hsl(60, 100%, 90%);
-}
-tr.next-section > * {
-	border-top: solid 3px black;
-}
-input.o {
-	width: 2em;
-	text-align: center;
-}
-input.regex {
-	font-family: monospace;
-	width: 24em;
-}
-input.expect {
-	width: 2em;
-	text-align: center;
-}
-input.color {
-	width: 4em;
-}
-
-.output {
-	font-family: monospace;
-	white-space: pre-wrap;
-}
-.output strong {
-	background: #eee;
-}
-</style>
+<link rel="stylesheet" href="/style.css" />
 </head>
 
 <body>
