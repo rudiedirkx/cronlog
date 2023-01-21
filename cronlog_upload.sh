475ed4b7cc0689488a876b63b191e62e81c2ea30
Rudie Dirkx <github@hotblocks.nl>
2023-01-21 02:01:15 +0100

    cronlog_upload cmd exec+

--- cronlog_upload.sh
+++ cronlog_upload.sh
@@ -7,6 +7,9 @@ OUTFILE=$TMP/cronlog_upload.out
 FULL_CMD="$@"
 OS_FROM="`whoami`@`hostname`"
 
-{ date; echo; time $FULL_CMD ; } &>$OUTFILE
+date >$OUTFILE
+echo >>$OUTFILE
+"$@" &>>$OUTFILE
+#{ date; echo; time $FULL_CMD ; } &>$OUTFILE
 
 curl -s -o /dev/null --form "subject=$FULL_CMD" --form "from=$OS_FROM" --form "body=@$OUTFILE" $CRONLOG_URL
