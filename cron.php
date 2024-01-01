e7366540f2fd94a99d0dee7fa02d4842d01c0035
Rudie Dirkx <github@hotblocks.nl>
2024-01-01 17:54:52 +0100

    -explicit notifications from mail

--- cron.php
+++ cron.php
@@ -90,7 +90,7 @@ $ydiff = $reader->results - $yesterdayNum;
 
 $log  = "";
 $log .= "{$reader->results} results{$skipped},\n";
-$log .= "{$reader->notifications}/{$reader->anominals} anominal,\n";
+$log .= "{$reader->anominals} anominal,\n";
 $log .= ($ydiff == 0 ? 'same as' : ($ydiff > 0 ? '+' : '-') . abs($ydiff) . ' from') . " yesterday\n\n";
 $log .= CRONLOG_URI . "/results.php?batch=" . $reader->batch . ($reader->anominals ? '&anominal=1' : '') . "\n\n";
 $log .= CRONLOG_URI . "/results-compare.php?source=batch&date1=" . $reader->batch . '&date2=' . $yesterdayBatch . "\n\n";
@@ -105,7 +105,7 @@ if ( count($reader->skipped) ) {
 }
 
 if ( CRONLOG_EMAIL_RESULTS ) {
-	$subject = "{$reader->results} cron results imported, {$reader->notifications}/{$reader->anominals} anominal";
+	$subject = "{$reader->results} cron results imported, {$reader->anominals} anominal";
 	mail(CRONLOG_EMAIL_RESULTS, $subject, $log, implode("\r\n", [
 		"From: Cronlog <cronlog@hotblocks.nl>",
 		"Return-path: cronlog@hotblocks.nl",
